dd78edbe7611826631ed51118e927e40b06556c7
Kuris <kurisotaku@gmail.com>
2022-07-08 16:33:41 -0300

    Import IMG

--- src/Pages/body.js
+++ src/Pages/body.js
@@ -1,23 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import logo from '../img/Logo.png';
+
+function ImgCar(img) {
+
+  return (<img src={img} />);
+}
+
 function Body() {
 
-const axios = require('axios');
+  const imgLogo = ImgCar(logo);
+  const axios = require('axios');
 
-const sendGetRequest = async () => {
+  const [imgId, setImgId] = useState(imgLogo);
+  const [listaCars, setlistaCars] = useState([]);
+  const sendGetRequest = async () => {
+    let resp;
     try {
-        const resp = await axios.get('https://jkflix.herokuapp.com/carros');
-        console.log(resp.data);
+      resp = await axios.get('https://jkflix.herokuapp.com/carros');
+      setlistaCars(resp.data);
+      //console.log(resp.data);
     } catch (err) {
-        // Handle Error Here
-        console.error(err);
+      // Handle Error Here
+      console.error(err);
     }
-};
-sendGetRequest();
+    return resp.data;
+  };
 
-    return (
-      <div>
-        <p>{axios}</p>
-      </div>
-    );
-  }
+  sendGetRequest().then((resp) => {
+
+    importarImg(resp);
+
+  });
+
+
+  /*
+    function getTest() {
   
-  export default Body;
+      return fetch(`https://jkflix.herokuapp.com/carros`).then(async (respostaServidor) => {
+        if (respostaServidor.ok) {
+          const resposta = await respostaServidor.json();
+          return resposta;
+        }
+        console.log(respostaServidor);
+        throw new Error('Não foi possível pegar os dados');
+      });
+    }
+  
+    useEffect(() => {
+      getTest()
+        .then((list) => {
+          console.log(list)
+          import(`../img/${list[0].id}.jpg`).then(image => {
+            setImgId(image);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+  */
+
+  return (
+    <div>
+    </div>
+  );
+
+    
+
+  function importarImg(resp) {
+    import(`../img/${resp[0].id}.jpg`).then(image => {
+      setImgId(ImgCar(image.default));
+    });
+  }
+}
+
+export default Body;
